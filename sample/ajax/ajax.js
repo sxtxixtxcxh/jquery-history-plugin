4078e5c7270de80b96e9c218ba1b2451d5788b26
Takayuki Miwa <i@tkyk.name>
2010-05-18 23:30:59 +0900

    Added AJAX sample

---
+++ sample/ajax/ajax.js
@@ -0,0 +1,18 @@
+
+jQuery(document).ready(function($) {
+        function load(num) {
+            $('#content').load(num +".html");
+        }
+
+        $.history.init(function(url) {
+                load(url == "" ? "1" : url);
+            });
+
+        $('#ajax-links a').live('click', function(e) {
+                var url = $(this).attr('href');
+                url = url.replace(/^.*#/, '');
+                load(url);
+                $.history.load(url);
+                return false;
+            });
+    });
